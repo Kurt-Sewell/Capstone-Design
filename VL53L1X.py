888a29efd424d5f5beb4f19fb838b6d57bb1380c
Kurt-Sewell <awesomedef9@gmail.com>
2025-11-14 18:51:32 -0600

    Much smarter way of implementing ToF sensors.

--- VL53L1X.py
+++ VL53L1X.py
@@ -4,33 +4,48 @@ import board
 import busio
 import adafruit_tca9548a
 from adafruit_vl53l1x import VL53L1X
+import numpy as np
 
 i2c = busio.I2C(board.SCL, board.SDA) # uses board.SCL and board.SDA
 tca = adafruit_tca9548a.TCA9548A(i2c)
-vl1 = VL53L1X(tca[0])
-vl2 = VL53L1X(tca[1])
-# vl3 = VL53L1X(tca[2])
-# vl4 = VL53L1X(tca[3])
-# vl5 = VL53L1X(tca[4])
-# vl6 = VL53L1X(tca[5])
-# vl7 = VL53L1X(tca[6])
-# vl8 = VL53L1X(tca[7])
-
-def distance(sensor):
-    """Get distance reading from a VL53L1X sensor with error handling."""
+tof = [None] * 8
+ch = 2
+for i in range(ch):
     try:
-        return sensor.distance
-    except RuntimeError:
-        return None
+        tof[i] = VL53L1X(tca[i])
+        tof[i].start_ranging()
+        tof[i].timing_budget = 500
+        print(tof[i], "At position", i)
+    except:
+        pass
+# vl1 = VL53L1X(tca[0])
+# vl2 = VL53L1X(tca[1])
+# # vl3 = VL53L1X(tca[2])
+# # vl4 = VL53L1X(tca[3])
+# # vl5 = VL53L1X(tca[4])
+# # vl6 = VL53L1X(tca[5])
+# # vl7 = VL53L1X(tca[6])
+# # vl8 = VL53L1X(tca[7])
+
+# def distance(sensor):
+#     """Get distance reading from a VL53L1X sensor with error handling."""
+#     try:
+#         return sensor.distance
+#     except RuntimeError:
+#         return None
     
-print("Timing Budget: {}".format(vl1.timing_budget))
-vl1.start_ranging()
-vl2.start_ranging()
+print("Timing Budget: {}".format(tof[0].timing_budget))
+# vl1.start_ranging()
+# vl2.start_ranging()
     
 while True:
-    print("Distance 1: {} cm".format(distance(vl1)))
-    print("Distance 2: {} cm".format(distance(vl2)))
-    vl1.clear_interrupt
-    vl2.clear_interrupt 
-    # "Distance 3: {} cm".format(distance(vl3)), "Distance 4: {} cm".format(distance(vl4)), "Distance 5: {} cm".format(distance(vl5)), "Distance 6: {} cm".format(distance(vl6)), "Distance 7: {} cm".format(distance(vl7)), "Distance 8: {} cm".format(distance(vl8)))
-    time.sleep(1)
+    for i in range(ch):
+        print("Distance", i, ": {} cm".format(tof[i].distance))
+        tof[i].clear_interrupt()
+    time.sleep(tof[0].timing_budget * .001)
+#     print("Distance 1: {} cm".format(distance(vl1)))
+#     print("Distance 2: {} cm".format(distance(vl2)))
+#     vl1.clear_interrupt
+#     vl2.clear_interrupt 
+#     # "Distance 3: {} cm".format(distance(vl3)), "Distance 4: {} cm".format(distance(vl4)), "Distance 5: {} cm".format(distance(vl5)), "Distance 6: {} cm".format(distance(vl6)), "Distance 7: {} cm".format(distance(vl7)), "Distance 8: {} cm".format(distance(vl8)))
+#     time.sleep(1)
